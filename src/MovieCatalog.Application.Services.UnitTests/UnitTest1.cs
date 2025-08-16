3ab6f03e29fb8ddd7a0606c01e46170c37ca3850
Coretex <valerash2@gmail.com>
2025-08-16 09:15:35 +0300

    init

---
+++ src/MovieCatalog.Application.Services.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCatalog.Application.Services.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/MovieCatalog.Providers.Omdb.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCatalog.Providers.Omdb.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
